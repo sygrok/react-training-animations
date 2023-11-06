254fae2a63d7451b0595e2690f3db14ac2c359a0
Berkay Anduv <sygrok@hotmail.com>
2023-11-07 01:42:13 +0300

    NestedAnimations&StaggredLists

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,14 +9,15 @@ export default function Modal({ title, children, onClose }) {
         variants={{
           hidden: { opacity: 0, y: -30 },
           visible: { opacity: 1, y: 0 },
+          exit: { opacity: 0, y: -30 },
         }}
+        transition={{ duration: 0.5 }}
         initial="hidden"
         animate="visible"
-        transition="hidden"
+        exit="hidden"
         // initial={{ opacity: 0, y: -30 }}
         // animate={{ opacity: 1, y: 0 }}
-        // transition={{ duration: 0.5 }}
-        exit={{ opacity: 0, y: -30 }} //to make it work we've made some changes in header.jsx
+        // exit={{ opacity: 0, y: -30 }} //to make it work we've made some changes in header.jsx
         open
         className="modal"
       >
